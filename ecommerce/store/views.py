222c04330f4e764452f5c3bf99c9f3204e910324
CodeCraftKiran <codecraftkiran@gmail.com>
2024-08-13 21:11:45 +0530

    models and main page is ready

---
+++ ecommerce/store/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Category, Product
+# Create your views here.
+
+def Store(request):
+    all_products = Product.objects.all()
+    context = {'my_products': all_products}
+    return render(request, 'store/store.html', context)
+
+
+def categories(request):
+    all_categories = Category.objects.all()
+    return {'all_categories': all_categories}
+    # This function is added in settings.py -> context_processors so that it can be available to all the pages.
+    
+    
