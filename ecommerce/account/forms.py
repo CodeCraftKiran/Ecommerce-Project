c40de4ce32d8711af096802240891e697c9c95fd
CodeCraftKiran <codecraftkiran@gmail.com>
2024-08-25 22:09:07 +0530

    email verify start

--- ecommerce/account/forms.py
+++ ecommerce/account/forms.py
@@ -12,11 +12,17 @@ class CreateUserForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(CreateUserForm, self).__init__( *args, **kwargs)
         
+        # Mark email as required
+        self.fields['email'].required = True
+        
         
     def clean_email(self):
         email = self.cleaned_data.get('email')
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError('This email is invalid')
-        if len(email >= 350):
+        
+        # len function updated ###
+        if len(email) >= 350:
             raise forms.ValidationError('Your email is too long.')
             
+        return email
--- ecommerce/account/views.py
+++ ecommerce/account/views.py
@@ -11,8 +11,24 @@ def register(request):
         
         if form.is_valid():
             form.save()
-            return redirect('')
+            return redirect('store')
         
     context = {'form': form}
     
     return render(request, 'account/registration/register.html', context=context)
+
+
+def email_verification(request):
+    pass
+
+
+def email_verification_success(request):
+    pass
+
+
+def email_verification_failed(request):
+    pass
+
+
+def email_verification_sent(request):
+    pass
