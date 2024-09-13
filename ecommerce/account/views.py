261002657b1f01143e3b0622952bbda19e4426fd
CodeCraftKiran <codecraftkiran@gmail.com>
2024-09-13 21:54:55 +0530

    UI/UX improved

--- ecommerce/account/views.py
+++ ecommerce/account/views.py
@@ -15,6 +15,7 @@ from django.contrib.auth import authenticate, login, logout
 
 from django.contrib.auth.decorators import login_required
 
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
@@ -102,7 +103,16 @@ def my_login(request):
 # Logout
 
 def user_logout(request):
-    auth.logout(request)
+    try:
+        for key in list(request.session.keys()):
+            if key == 'session_key':
+                continue
+            else:
+                del request.session[key]
+    except KeyError:
+        pass        
+    
+    messages.success(request, "Logout success")
     return redirect('store')
 
 
@@ -120,6 +130,7 @@ def profile_management(request):
         user_form = UpdateUserForm(request.POST, instance=request.user)
         if user_form.is_valid():
             user_form.save()
+            messages.info(request, "Account updated")       
             return redirect('dashboard')
         
     context = {'user_form': user_form}
@@ -131,5 +142,7 @@ def delete_account(request):
     user = User.objects.get(id=request.user.id)
     if request.method == 'POST':
         user.delete()
+        
+        messages.error(request, "Account deleted")       
         return redirect('store')
     return render(request, 'account/delete-account.html')
